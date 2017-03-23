57d2c777ca5e9301a58b0fe7d212df600b9ac5c9
Nicholas Delpino <NBDelpino4@gmail.com>
2017-03-23 15:57:33 -0400

    minor adjuctments, still work in progress getting it fully mobile responsive and some style adjustments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 $( document ).ready(function(){
 
+    // NAVBAR SLIDE OUT
+    $(".button-collapse").sideNav();
+
     // SLIDER
     $('.slider').slider({full_width: true});
 
@@ -8,26 +11,29 @@ $( document ).ready(function(){
 
     // ENABLE TAB STYLING WHEN IN USE
     $('#main-menu ul li a').click(function (ev) {
+
         $('#main-menu ul li').removeClass('selected');
         $(ev.currentTarget).parent('li').addClass('selected');
-    });
 
-    // NAVBAR SLIDE OUT
-    $(".button-collapse").sideNav();
+    });
 
     // DATE PICKER
     $('.datepicker').pickadate({
+
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15 // Creates a dropdown of 15 years to control year
+
     });
 
     // AUTO COMPLETE
     $('input.autocomplete').autocomplete({
+
         data: {
             "Apple": null,
             "Microsoft": null,
             "Google": 'http://placehold.it/250x250'
         }
+
     });
 
     // BOX DROP DOWN SELECTION
